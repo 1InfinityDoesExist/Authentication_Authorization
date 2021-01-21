74c804f0b10d73b6755f6a764b126a3ff8982f66
Avinash Patel <avinash.patel@gaiansolutions.com>
2021-01-21 23:16:31 +0530

    refactor

--- oauth2_27/src/main/java/security/oauth2_27/controller/HomeController.java
+++ oauth2_27/src/main/java/security/oauth2_27/controller/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import security.oauth2_27.service.ProductService;
@@ -15,8 +16,8 @@ public class HomeController {
 	@Autowired
 	private ProductService productService;
 
-	@GetMapping("/products")
-	public List<String> findProductForUserName(Authentication a) {
-		return productService.findProductsForUser(a.getName());
+	@GetMapping("/products/{username}")
+	public List<String> findProductForUserName(Authentication a, @PathVariable(value = "username") String username) {
+		return productService.findProductsForUser(username);
 	}
 }
