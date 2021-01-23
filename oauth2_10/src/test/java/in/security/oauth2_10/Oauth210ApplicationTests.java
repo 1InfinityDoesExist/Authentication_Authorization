2fa902a4bc9610ebd5fca4960bd11f6c1593b36d
Avinash Patel <avinash.patel@gaiansolutions.com>
2021-01-23 23:10:12 +0530

    refactor cors configuration

---
+++ oauth2_10/src/test/java/in/security/oauth2_10/Oauth210ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.security.oauth2_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth210ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
